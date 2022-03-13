54bec8850142a3ddf075060774783b0382929224
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2022-03-13 16:19:52 +0800

    Add solution verification and chainlink vrf

--- contracts/KewlKevinPuzzle.sol
+++ contracts/KewlKevinPuzzle.sol
@@ -5,8 +5,11 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract KewlKevinPuzzle is ERC721, ReentrancyGuard, Ownable {
+
+contract KewlKevinPuzzle is VRFConsumerBase, ERC721, ReentrancyGuard, Ownable {
     using Counters for Counters.Counter;
 
     struct Coordinates {
@@ -40,7 +43,10 @@ contract KewlKevinPuzzle is ERC721, ReentrancyGuard, Ownable {
 
     uint constant SECONDS_DEADLINE = 960;
 
-    uint256 public randomNumber = 50;
+    bytes32 internal keyHash;
+    uint256 internal fee;
+
+    uint256 public randomNumber = 42069;
     address[] public nftPartners;
 
     uint256 constant UP = 0;
@@ -48,7 +54,13 @@ contract KewlKevinPuzzle is ERC721, ReentrancyGuard, Ownable {
     uint256 constant DOWN = 2;
     uint256 constant LEFT = 3;
 
-    constructor() ERC721('Kewl Kevin Puzzle', 'KKP') {}
+    constructor() ERC721('Kewl Kevin Puzzle', 'KKP') VRFConsumerBase(
+        0x3d2341ADb2D31f1c5530cDC622016af293177AE0, // VRF Coordinator
+        0xb0897686c545045aFc77CF20eC7A532E3120E0F1  // LINK Token
+    ) {
+        keyHash = 0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da;
+        fee = 0.00001 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
 
     function collatzNext(uint256 x) internal pure returns (uint256 result) {
         if (x % 2 == 0) {
@@ -117,6 +129,24 @@ contract KewlKevinPuzzle is ERC721, ReentrancyGuard, Ownable {
         }
         distance = 1 + rawDistance % (PUZZLE_WIDTH - cursor.x - 1);
         return Coordinates(_min(PUZZLE_WIDTH - 1, cursor.x + distance), cursor.y);
+    }
+
+    function _bounceBackWall(Coordinates memory playerPosition, uint256 direction)
+    internal
+    pure
+    returns (Coordinates memory)
+    {
+        if (direction == UP) {
+            return Coordinates(playerPosition.x, PUZZLE_HEIGHT - 1);
+        }
+        if (direction == DOWN) {
+            return Coordinates(playerPosition.x, 0);
+        }
+        if (direction == RIGHT) {
+            return
+            Coordinates(PUZZLE_WIDTH - 1, playerPosition.y);
+        }
+        return Coordinates(0, playerPosition.y);
     }
 
     function _bounceBackCursor(Coordinates memory obstacle, uint256 direction)
@@ -208,12 +238,71 @@ contract KewlKevinPuzzle is ERC721, ReentrancyGuard, Ownable {
             totalBalance += ERC721(nftPartners[i]).balanceOf(msg.sender);
             if (totalBalance > 0) break;
         }
-        require(totalBalance > 0, "KKP: You don't have an associated NFT with this smart contract.");
+        // require(totalBalance > 0, "KKP: You don't have an associated NFT with this smart contract.");
 
         minted.increment();
         _safeMint(msg.sender, minted.current());
 
         mintTimestamps[minted.current()] = block.timestamp;
+    }
+
+    function _findObstacleIndex(Coordinates memory cursor, Coordinates[] memory obstacles, uint256 puzzleLength, uint256 direction) internal pure returns (uint256) {
+        uint256 closestObstacleIndex = 64;
+
+        for (uint256 i = 0; i < puzzleLength; i++) {
+            Coordinates memory obstacle = obstacles[i];
+            if ((direction == UP || direction == DOWN) && obstacle.x != cursor.x) {
+                continue;
+            }
+            if ((direction == LEFT || direction == RIGHT) && obstacle.y != cursor.y) {
+                continue;
+            }
+
+            if (direction == UP && obstacle.y > cursor.y) continue;
+            if (direction == DOWN && obstacle.y < cursor.y) continue;
+            if (direction == LEFT && obstacle.x > cursor.x) continue;
+            if (direction == RIGHT && obstacle.x < cursor.x) continue;
+
+            if (closestObstacleIndex == 64) {
+                closestObstacleIndex = i;
+                continue;
+            }
+
+            if (direction == UP && obstacle.y < obstacles[closestObstacleIndex].y) continue;
+            if (direction == DOWN && obstacle.y > obstacles[closestObstacleIndex].y) continue;
+            if (direction == LEFT && obstacle.x < obstacles[closestObstacleIndex].x) continue;
+            if (direction == RIGHT && obstacle.x > obstacles[closestObstacleIndex].x) continue;
+
+            closestObstacleIndex = i;
+        }
+
+        return closestObstacleIndex;
+    }
+
+    function isCorrectSolution(uint256 tokenId, uint256[] memory movements) public view returns (bool) {
+        // require(movements.length != 0, 'KKP: Solution required.');
+
+        Puzzle memory burnPuzzle = _generatePuzzle(tokenId + randomNumber);
+
+        Coordinates memory cursor = Coordinates(50, 61);
+        Coordinates[] memory obstacles = burnPuzzle.obstacles;
+        uint256 puzzleLength = burnPuzzle.puzzleLength;
+        uint256 exitX = burnPuzzle.exitX;
+
+        for (uint256 i = 0; i < movements.length; i++) {
+            uint256 obstacleIndex = _findObstacleIndex(cursor, obstacles, puzzleLength, movements[i]);
+
+            if (obstacleIndex == 64) {
+                cursor = _bounceBackWall(cursor, movements[i]);
+            } else {
+                cursor = _bounceBackCursor(obstacles[obstacleIndex], movements[i]);
+                obstacles[obstacleIndex] = Coordinates(64, 64);
+            }
+        }
+
+        require(exitX == cursor.x, string(abi.encodePacked("Expected: ", Strings.toString(exitX), " Actual: ", Strings.toString(cursor.x))));
+
+        return exitX == cursor.x;
     }
 
     function burnAndClaimReward(uint256 tokenId, uint256[] memory movements)
@@ -233,9 +322,7 @@ contract KewlKevinPuzzle is ERC721, ReentrancyGuard, Ownable {
             return;
         }
 
-        require(movements.length != 0, 'KKP: Solution required.');
-
-        // TODO: require that movements do solve the puzzle
+        require(isCorrectSolution(tokenId, movements), "KKP: Your solution did not solve the puzzle.");
 
         require(currentSupply() > 0, "KKP: Fatal error! Supply Tokens == Burned Tokens.");
 
@@ -244,6 +331,21 @@ contract KewlKevinPuzzle is ERC721, ReentrancyGuard, Ownable {
 
         // reward player
         payable(msg.sender).transfer(address(this).balance / currentSupply());
+    }
+
+    /**
+     * Requests randomness
+     */
+    function getRandomNumber() public onlyOwner returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomNumber = randomness;
     }
 
     function setNftPartners(address[] memory newNftPartners) public onlyOwner {
